9ca737b72cd983d8981791dec806270863af502d
Deniz Tetik <deniztetik31@gmail.com>
2019-02-08 02:06:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,87 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 exports.default = void 0;
 
 var _react = require("react");
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
-  }
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-function _asyncToGenerator(fn) {
-  return function() {
-    var self = this,
-      args = arguments;
-    return new Promise(function(resolve, reject) {
-      var gen = fn.apply(self, args);
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-      }
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-      }
-      _next(undefined);
-    });
-  };
-}
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-function _slicedToArray(arr, i) {
-  return (
-    _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest()
-  );
-}
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _nonIterableRest() {
-  throw new TypeError("Invalid attempt to destructure non-iterable instance");
-}
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) {
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _e = undefined;
-  try {
-    for (
-      var _i = arr[Symbol.iterator](), _s;
-      !(_n = (_s = _i.next()).done);
-      _n = true
-    ) {
-      _arr.push(_s.value);
-      if (i && _arr.length === i) break;
-    }
-  } catch (err) {
-    _d = true;
-    _e = err;
-  } finally {
-    try {
-      if (!_n && _i["return"] != null) _i["return"]();
-    } finally {
-      if (_d) throw _e;
-    }
-  }
-  return _arr;
-}
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _arrayWithHoles(arr) {
-  if (Array.isArray(arr)) return arr;
-}
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var usePromise = function usePromise(fn) {
-  var watch =
-    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var watch = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-  var _useState = (0, _react.useState)(null),
-    _useState2 = _slicedToArray(_useState, 2),
-    loading = _useState2[0],
-    setLoading = _useState2[1];
+  var _useState = (0, _react.useState)(true),
+      _useState2 = _slicedToArray(_useState, 2),
+      loading = _useState2[0],
+      setLoading = _useState2[1];
 
   var _useState3 = (0, _react.useState)(null),
-    _useState4 = _slicedToArray(_useState3, 2),
-    result = _useState4[0],
-    setResult = _useState4[1];
+      _useState4 = _slicedToArray(_useState3, 2),
+      result = _useState4[0],
+      setResult = _useState4[1];
 
   var _useState5 = (0, _react.useState)(null),
-    _useState6 = _slicedToArray(_useState5, 2),
-    error = _useState6[0],
-    setError = _useState6[1];
+      _useState6 = _slicedToArray(_useState5, 2),
+      error = _useState6[0],
+      setError = _useState6[1];
 
   var refresh =
+  /*#__PURE__*/
+  function () {
+    var _ref = _asyncToGenerator(
     /*#__PURE__*/
-    (function() {
-      var _ref = _asyncToGenerator(
-        /*#__PURE__*/
-        regeneratorRuntime.mark(function _callee() {
-          var res;
-          return regeneratorRuntime.wrap(
-            function _callee$(_context) {
-              while (1) {
-                switch ((_context.prev = _context.next)) {
-                  case 0:
-                    _context.prev = 0;
-                    _context.next = 3;
-                    return fn();
+    regeneratorRuntime.mark(function _callee() {
+      var res;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.prev = 0;
+              _context.next = 3;
+              return fn();
 
-                  case 3:
-                    res = _context.sent;
-                    setResult(res);
-                    _context.next = 10;
-                    break;
+            case 3:
+              res = _context.sent;
+              setResult(res);
+              _context.next = 10;
+              break;
 
-                  case 7:
-                    _context.prev = 7;
-                    _context.t0 = _context["catch"](0);
-                    setError(_context.t0);
+            case 7:
+              _context.prev = 7;
+              _context.t0 = _context["catch"](0);
+              setError(_context.t0);
 
-                  case 10:
-                    _context.prev = 10;
-                    setLoading(false);
-                    return _context.finish(10);
+            case 10:
+              _context.prev = 10;
+              setLoading(false);
+              return _context.finish(10);
 
-                  case 13:
-                  case "end":
-                    return _context.stop();
-                }
-              }
-            },
-            _callee,
-            this,
-            [[0, 7, 10, 13]],
-          );
-        }),
-      );
+            case 13:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee, this, [[0, 7, 10, 13]]);
+    }));
 
-      return function refresh() {
-        return _ref.apply(this, arguments);
-      };
-    })();
+    return function refresh() {
+      return _ref.apply(this, arguments);
+    };
+  }();
 
-  (0, _react.useEffect)(function() {
+  (0, _react.useEffect)(function () {
     refresh();
   }, watch);
   return [loading, result, error, refresh];
