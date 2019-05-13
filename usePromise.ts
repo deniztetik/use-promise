67f30c6b6667686f090f8fce33e8004ed5d4a28f
Deniz Tetik <deniztetik31@gmail.com>
2019-05-13 19:13:35 -0400

    Get rid of unused interface

--- usePromise.ts
+++ usePromise.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const usePromise = (fn: () => any, watch = []): IUsePromise => {
+const usePromise = (fn: () => any, watch = []) => {
   const [loading, setLoading] = useState(true);
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
